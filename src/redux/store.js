a0b3ffbfada588f2a4855328ed16dfb1950d15f1
Emil-Bayramov <105307406+Emil-Bayramov@users.noreply.github.com>
2023-01-30 09:55:39 -0600

    hw6

---
+++ src/redux/store.js
@@ -0,0 +1,33 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+
+import contactsReducer from './contacts/contactsSlice';
+import filterReducer from './filter/filterSlice';
+
+const persistConfig = {
+  key: 'contacts',
+  storage,
+  blacklist: ['filter'],
+};
+const persistedReducer = persistReducer(persistConfig, contactsReducer);
+
+export const store = configureStore({
+  reducer: { contacts: persistedReducer, filter: filterReducer },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+export const persistor = persistStore(store);
